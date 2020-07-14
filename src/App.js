08409daf4bfa0272029b729f357234ba0257e485
sesirimarco <sesirimarco@gmail.com>
2020-07-13 23:16:01 -0300

    refactoring  of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import AddTodoUI from './components/addTodoUI/';
+import FiltersTodoUI from './components/filtersTodoUI/';
+import ListTodoUI from './components/listTodoUI/';
 import {
 	addTodo,
 	completeTodo,
@@ -7,37 +10,16 @@ import {
 	visibilityFilters,
 } from './actions';
 
-const getFilteredTodos = (todos, selectedFilter) => {
-	switch(selectedFilter) {
-		case visibilityFilters.SHOW_COMPLETE:
-			return todos.filter(todo => todo.completed);
-		case visibilityFilters.SHOW_ACTIVE:
-			return todos.filter(todo => !todo.completed);
-		default:
-			return todos;
-	}
-};
-const parseFilterName = (filter) => {
-	return filter.split('_')
-		.map((word, index) => {
-			return (index === 0) 
-				? `${word.charAt(0)}${word.slice(1).toLowerCase()} `
-				: `${word.toLowerCase()}`
-		})
-};
 function App() {
-	const [value, setInputValue] = useState('');
 	const dispatch = useDispatch();
-	const todos = useSelector(state => state.todos);
-	const selectedFilter = useSelector(state => state.visibilityFilter);
-	let filteredsTodos = getFilteredTodos(todos, selectedFilter);
 	const addNewTask = (value) => {
 		if(value.length > 3) {
 			dispatch(addTodo(value));
-			setInputValue('');
 		}
-		
 	};
+	const todos = useSelector(state => state.todos);
+	const selectedFilter = useSelector(state => state.visibilityFilter);
+		
 	return (
 		<div className="container">
 			<div className="row justify-content-center">
@@ -45,80 +27,36 @@ function App() {
 					<h2 className="display-5 mt-3">Notes with Redux</h2>
 					<hr />
 					<div className="input-group mt-3">
-						<input 
-							id="newTask" 
-							className="form-control" 
-							type="text" 
-							placeholder="Add a new task..."
-							value={value}
-							onChange={e => setInputValue(e.target.value)}
-							onKeyDown={(e) => {
-								if(e.key === 'Enter') {
-									addNewTask(value);
-								}
+						<AddTodoUI
+							addNewTask={(value) => {
+								addNewTask(value);
 							}}
-						></input>
-						<div>
-							<button 
-								className="btn btn-outline-success" 
-								onClick={() => {
-									addNewTask(value);
-								}}
-							>
-								add task
-							</button>
-						</div>
+						/>
 					</div>
 				</div>
 			</div>
 			<div className="row justify-content-center">
 				<div className="col-9 col-sm-9 col-md-6">
 					<div className="input-group mt-3">
-						<select onChange={(e) => {
-							dispatch(setVisibilityFilter(e.currentTarget.value))
+						<FiltersTodoUI 
+							onChange={(filter) => {
+								dispatch(setVisibilityFilter(filter));
 							}}
-							className="custom-select"
-						>
-						{
-							Object.values(visibilityFilters)
-								.map((filter, index) => 
-									<option 
-										key={index}
-										value={filter}
-									>
-										{ 
-											parseFilterName(filter)
-										}
-									</option>
-								)
-						}
-						</select>								
+							filters={visibilityFilters}
+						/>							
 					</div>
 				</div>
 			</div>
 			<div className="row justify-content-center">
 				<div className="col-9 col-sm-9 col-md-6">
-						{filteredsTodos.length > 0 &&
-							<ul className="list-group mt-3">
-								{filteredsTodos.map((todo) => 
-									<li 
-										className="list-group-item d-flex justify-content-between align-items-center" 
-										key={todo.id}
-									>
-										{todo.text}
-										{!todo.completed &&
-											<button 
-												className="btn btn-outline-primary" 
-												onClick={() => {
-													dispatch(completeTodo(todo.id))
-												}}>
-													Done
-											</button>
-										}
-									</li>
-								)}
-							</ul>
-						}
+					<ListTodoUI
+						todos={todos}
+						selectedFilter={selectedFilter}
+						filters={visibilityFilters}
+						onClick={(id) => {
+							dispatch(completeTodo(id));
+						}}
+					/>
 					</div>
 				</div>
 		</div>
---
+++ src/components/addTodoUI/todoInput.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TodoInput = (props) => {
+    console.log('props ' , props)
+    return (
+        <input 
+            id="newTask" 
+            className="form-control" 
+            type="text" 
+            placeholder="Add a new task..."
+            value={props.value}
+            onChange={e => props.setValue(e.target.value)}
+            onKeyDown={(e) => {
+                if(e.key === 'Enter') {
+                    props.addNewTask();
+                }
+            }}
+        ></input>
+    );
+};
+
+export default TodoInput;
